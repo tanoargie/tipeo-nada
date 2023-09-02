32cdfdc59322930ddc9c3c686a77f38feb7f7deb
Franco Serio <francoagustinserio@gmail.com>
2023-09-02 15:17:34 -0300

    Assets path

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -40,7 +40,7 @@ Game::Game(const char *title) {
     if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) {
       printf("Error opening music: %s\n", Mix_GetError());
     } else {
-      backgroundMusic = Mix_LoadMUS("../src/Assets/cyberpunk.mp3");
+      backgroundMusic = Mix_LoadMUS("Assets/cyberpunk.mp3");
       Mix_VolumeMusic((20 * MIX_MAX_VOLUME) / 100);
 
       if (!backgroundMusic) {
@@ -60,7 +60,7 @@ Game::Game(const char *title) {
       return;
     }
 
-    font = TTF_OpenFont("../src/Assets/OpenSans-Bold.ttf", 24);
+    font = TTF_OpenFont("Assets/OpenSans-Bold.ttf", 24);
     if (font == NULL) {
       cout << SDL_GetError();
     }
@@ -71,7 +71,7 @@ Game::Game(const char *title) {
 
     srand(time(0));
 
-    ifstream wordfile("../src/Assets/words.txt");
+    ifstream wordfile("Assets/words.txt");
     string line;
 
     while (getline(wordfile, line)) {
